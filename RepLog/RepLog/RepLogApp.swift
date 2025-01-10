c7147df14427df1c79b6d2e4ce5a717eabf1b24f
BradleyCurci <116754035+BradleyCurci@users.noreply.github.com>
2025-01-10 10:33:27 -0500

    init

---
+++ RepLog/RepLog/RepLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepLogApp.swift
+//  RepLog
+//
+//  Created by Brad Curci on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RepLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RepLog/RepLogTests/RepLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  RepLogTests.swift
+//  RepLogTests
+//
+//  Created by Brad Curci on 1/10/25.
+//
+
+import Testing
+@testable import RepLog
+
+struct RepLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
